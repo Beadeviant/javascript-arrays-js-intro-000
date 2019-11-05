0feecbdcb43ea3dfaa5698dba73d78d5a067282e
lite-closure-2800 <bri.barden@gmail.com>
2019-11-05 14:48:04 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,31 @@
+var chocolateBars = [
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles"
+]
+function addElementToBeginningOfArray(array, element){
+var newArray = [element, ...array]
+return newArray
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+array.unshift(element)
+return array
+}
+function addElementToEndOfArray(array, element){
+  var endArray =[...array, element]
+  return endArray
+}
+function destructivelyAddElementToEndOfArray(array, element){
+array.push(element)
+return array
+}
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+}
+function destructivelyRemoveElementFromEndOfArray(array, element){
+  array.pop(element)
+  return array
+}
